5d4d72b2d62e7324f486015da4acfed5e09f37f4
Edilma Riano <edilmaria@hotmail.com>
2020-01-29 06:32:18 -0500

    Working in user and maintenance class

---
+++ Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoTracker.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public int PhoneNumber { get; set; }
+
+        public User() { }
+        public User (string username, string password, string email)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+        }
+
+
+    }
+}
