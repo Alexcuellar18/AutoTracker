a34c690f5f11a804a941774fcb0088409ecc11bd
Edilma Riano <edilmaria@hotmail.com>
2020-02-03 16:47:46 -0500

    working with user registration form - validation.  NO complete

--- ViewModel/AddUserViewModel.cs
+++ ViewModel/AddUserViewModel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using AutoTracker.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace AutoTracker.ViewModels
 {
@@ -12,21 +14,30 @@ namespace AutoTracker.ViewModels
         public string Username { get; set; }
         [Required]
         public string Email { get; set; }
+        
+        [Required(ErrorMessage ="Please enter your name")]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+       
         [Required]
         public string Password { get; set; }
-        [Required(ErrorMessage = "Passwords do not match")]
 
+        [Required(ErrorMessage = "Passwords do not match")]
         [Display(Name = "Verify Password")]
         [Compare("Password")]
         public string Verify { get; set; }
-        public string Name { get; set; }
 
+        [Required(ErrorMessage ="Zip Code can not be empty")]
         [RegularExpression(@"^[0-9]{5}$",
          ErrorMessage = "Characters are not allowed.")]
-        [Required]
+        [Display(Name="Zip Code")]
         public string ZipCode { get; set; }
-        public string LastName { get; set; }
-        
+
+        public AddUserViewModel() 
+        { }
 
 }
 }
