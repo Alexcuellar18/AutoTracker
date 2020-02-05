8510dc8a34aa4674e29afdf1820aad7297dcd1a1
Alex C <alejandrocuellar18@gmail.com>
2020-02-04 19:25:17 -0500

    class creation

---
+++ Models/Maintenance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutoTracker.Models
+{
+    public class Maintenance
+    {
+        public string ServiceDescription { get; set; }
+        public double ServiceCost { get; set; }
+
+        public DateTime ServiceDate { get; set; }
+
+
+
+    }
+}
