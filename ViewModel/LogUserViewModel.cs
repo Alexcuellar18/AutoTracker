a9ce392979d5e640a031e0590ba2145289568c2a
Edilma <edilmaria@hotmail.com>
2020-02-09 17:57:00 -0500

    Create logUserViewModel and use it in controller

---
+++ ViewModel/LogUserViewModel.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+
+namespace AutoTracker.ViewModels
+{
+    public class LogUserViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        public LogUserViewModel()
+        { }
+    }
+}
